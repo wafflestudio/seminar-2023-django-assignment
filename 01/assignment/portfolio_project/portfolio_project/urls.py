480ec4a9d9e9d2b0c1888f7ab0d4f41297a5a164
Chanuk Kim <cksdnr987@gmail.com>
2023-09-24 09:08:49 +0900

    01 assignment done

--- 01/assignment/portfolio_project/portfolio_project/urls.py
+++ 01/assignment/portfolio_project/portfolio_project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('portfolio.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("portfolio.urls")),
 ]

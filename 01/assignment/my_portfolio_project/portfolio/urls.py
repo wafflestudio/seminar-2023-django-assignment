0f5c399e7346a5f0c059dd1675a1422ec0f7730d
Hyeonoh <oasis2098@gmail.com>
2023-10-25 20:27:01 +0900

    assignment_done

--- 01/assignment/my_portfolio_project/portfolio/urls.py
+++ 01/assignment/my_portfolio_project/portfolio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PortfolioView.as_view()),
+    path('', views.index, name='index'),
 ]
--- 01/assignment/my_portfolio_project/portfolio/views.py
+++ 01/assignment/my_portfolio_project/portfolio/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
 
 # Create your views here.
-class PortfolioView(TemplateView):
-    template_name = 'portfolio/portfolio.html'
+def index(request):
+    return render(request, 'portfolio/portfolio.html')

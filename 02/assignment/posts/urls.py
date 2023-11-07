212fdbcda4b47925186b30905a2d26a98585a9e0
civilian38 <gyk23953@gmail.com>
2023-11-08 01:52:09 +0900

    CRUD 구현 완료

--- 02/assignment/posts/urls.py
+++ 02/assignment/posts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("posts/<int:post_id>/", views.detailView, name='post-detail'),
     path("posts/new/", views.createView, name='post-create'),
+    path("posts/<int:post_id>/update/", views.updateView, name='post-update'),
+    path("posts/<int:post_id>/delete/", views.deleteView, name='post-delete'),
 ]
--- 02/assignment/posts/views.py
+++ 02/assignment/posts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Post, Comment
+
+
 def index(request):
     postings = Post.objects.all()
     contents = dict()
     contents["posts"] = postings
     return render(request, "posts/index.html", context=contents)
+
 
 def detailView(request, post_id):
     posting = Post.objects.get(id=post_id)
@@ -13,11 +16,34 @@ def detailView(request, post_id):
     contents["comments"] = Comment.objects.filter(post_id=post_id)
     return render(request, 'posts/post_detail.html', context=contents)
 
+
 def createView(request):
-    if request.method == "POST":
-        title = request.POST["title"]
-        content = request.POST["content"]
-        post = Post.objects.create(title=title, content=content, author=request.user)
-        return redirect(f"/posts/{post.id}")
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        newPost = Post.objects.create(title=title, content=content, author=request.user)
+        return redirect(f"/posts/{newPost.id}/")
+    return render(request, "posts/post_form.html")
+
+
+def updateView(request, post_id):
+    if request.method == 'POST':
+        uppost = Post.objects.get(id = post_id)
+        uppost.title = request.POST['title']
+        uppost.content = request.POST['content']
+        uppost.save()
+        return redirect(f"/posts/{uppost.id}/")
     else:
-        return render(request, "posts/post_form.html")
+        post = Post.objects.get(id=post_id)
+        contents = dict()
+        contents["post_o"] = post
+    return render(request, "posts/post_update.html", context=contents)
+
+def deleteView(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('index')
+    else:
+        return render(request, 'posts/post_confirm_delete.html', {'post': post})
+

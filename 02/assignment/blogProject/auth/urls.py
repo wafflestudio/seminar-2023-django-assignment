0730e49a4af541323d3262218157056c6c00352a
nuagenic <nuagenic@gmail.com>
2023-11-08 01:35:02 +0900

    feat: Add 'create' architecture

--- 02/assignment/blogProject/auth/urls.py
+++ 02/assignment/blogProject/auth/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('create/', views.create, name='create'),
 ]
--- 02/assignment/blogProject/auth/views.py
+++ 02/assignment/blogProject/auth/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import UserForm
 
 
 # Create your views here.
@@ -8,3 +11,18 @@ class LoginView(auth_views.LoginView):
 
 class LogoutView(auth_views.LogoutView):
     template_name = "logout.html"
+
+
+def create(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('/auth/login/')
+    else:
+        form = UserForm()
+    return render(request, 'create.html', {'form': form})

6df12ac21d3269aef7a6d27383445fb001361cda
nuagenic <nuagenic@gmail.com>
2023-11-07 23:40:20 +0900

    feat: Add blog views and templates

--- 02/assignment/blogProject/blogProject/urls.py
+++ 02/assignment/blogProject/blogProject/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth.urls')),
+    path('blog/', include('blog.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]

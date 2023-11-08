d75d6470abd89caa70e507ab79f9670145b02901
nuagenic <nuagenic@gmail.com>
2023-11-08 19:41:28 +0900

    fix: Fix settings for rds connection

--- 02/assignment/blogProject/blogProject/settings.py
+++ 02/assignment/blogProject/blogProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3_0qo+hdo4oq^j!_-o85(3p$uo9t5!w1(k8p=yi0gm&j^1uhb$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['blogProject-env.eba-c2pdbmvy.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['blogProject3-env.eba-evmmsnih.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -82,12 +82,18 @@ WSGI_APPLICATION = 'blogProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
 
 
 # Password validation

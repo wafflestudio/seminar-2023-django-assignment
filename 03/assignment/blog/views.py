bb72e13f2a40cc8d81a1c3cb33a94894cf47fac4
civilian38 <gyk23953@gmail.com>
2023-11-18 23:57:08 +0900

    로그인 기능 구현 완료

--- 03/assignment/blog/views.py
+++ 03/assignment/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View, ListView
 
-# Create your views here.
+from .models import Post, Comment, TagPost, TagComment
+class IndexView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/index.html'
+    paginate_by = 8
+

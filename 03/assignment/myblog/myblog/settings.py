894d8b3cbdde046e8e3305b124ad83203a65423f
seoyeonnk <kkimseoyeon@snu.ac.kr>
2023-11-20 01:14:48 +0900

    1119 update

--- 03/assignment/myblog/myblog/settings.py
+++ 03/assignment/myblog/myblog/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+a1mkrkk%p&(uq@ijc)-gjm*7%119bqeilpr^8v&+zeyzhbm=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['myblog-dev.us-west-2.elasticbeanstalk.com',
-                 'ds2020-env.eba-dphauuwg.us-west-2.elasticbeanstalk.com']
+                 'ds2020-env.eba-dphauuwg.us-west-2.elasticbeanstalk.com',
+                 'my-env.eba-z22p8heh.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition

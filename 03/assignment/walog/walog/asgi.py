b52543c0c011fa0f3f6b085f41dff6ddf4c8b14e
JUNG SIHUN <gssihunjung@gmail.com>
2023-11-21 03:33:38 +0900

    waffle03
    블로그 rest framework

---
+++ 03/assignment/walog/walog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for walog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'walog.settings')
+
+application = get_asgi_application()

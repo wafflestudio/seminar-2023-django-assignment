10f9890944f21b48e36253ebe6dae4aa14520de2
civilian38 <gyk23953@gmail.com>
2023-11-21 02:26:47 +0900

    DRF 이용 시작
    DRF 프로젝트와 기존 프로젝트 분리

---
+++ 03/assignment_drf/blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey("account.User", on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, models.CASCADE)
+    created_by = models.ForeignKey("account.User", models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_updated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.content[:30]
+
+    class Meta:
+        ordering = ['-created_at']
+
+class TagPost(models.Model):
+    content = models.TextField(max_length=50, primary_key=True)
+    post = models.ManyToManyField("Post", related_query_name='tags', related_name='tagPost')
+
+    def __str__(self):
+        return self.content
+
+class TagComment(models.Model):
+    content = models.TextField(max_length=50, primary_key=True)
+    comment = models.ManyToManyField("Comment", related_query_name='tags', related_name='tagComment')
+
+    def __str__(self):
+        return self.content

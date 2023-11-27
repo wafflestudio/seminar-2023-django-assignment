f2c127a4d8c56bb055654ec41913f45d1d66f42b
JUNG SIHUN <66393246+20222-sh@users.noreply.github.com>
2023-11-27 21:05:44 +0900

    Update views.py

--- 03/assignment/walog/blog/views.py
+++ 03/assignment/walog/blog/views.py
@@ -47,24 +47,6 @@ def TagCheck():
             pass
         else:
             tag.delete()
-# def post_create(request):
-#     if request.method == "POST":
-#         form = PostForm(request.POST)
-#         form.instance.created_by = request.user
-#         if form.is_valid():
-#             if request.user.is_authenticated == False:
-#                 return redirect('/login/')
-#             post = form.save()
-#             tags = request.POST['tags']
-#             tags = tags.split(',')
-#             for tag in tags:
-#                 if not Tag.objects.filter(name=tag):
-#                     Tag.objects.create(name=tag)
-            
-#             return reverse('post-detail', kwargs={'pk' : post.id})
-#     form = PostForm()
-#     return render(request, 'blog/post_form.html', {'form' : form})
-
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     login_url = settings.LOGIN_URL
@@ -291,31 +273,7 @@ class APIPostDetail(LoginRequiredMixin, RetrieveUpdateDestroyAPIView):
         response = super().delete(request, *args, **kwargs)
         TagCheck()
         return response
-    #authentication_classes = [TokenAuthentication]
-    #permission_classes = [IsAuthenticated]
-
-    # def get_object(self, pk):
-    #     post = get_object_or_404(Post, pk=pk)
-    #     return post
-    
-    # def get(self, request, pk):
-    #     post = self.get_object(pk)
-    #     serializer = PostSerializer(post)
-    #     return Response(serializer.data)
-
-    # def patch(self, request, pk):
-    #     post = self.get_object(pk)
-    #     serializer = PostSerializer(post, data=request.data, partial=True)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # def delete(self, request, pk):
-    #     post = self.get_object(pk)
-    #     post.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-
+        
 class CommentPageNumberPagination(PageNumberPagination):
     page_size = 10
 

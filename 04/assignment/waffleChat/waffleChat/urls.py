672ffb78754bd3239f37558ad90816c1aca42de6
nuagenic <nuagenic@gmail.com>
2023-12-13 04:36:28 +0900

    feat: Apply swagger-ui(refacted)

--- 04/assignment/waffleChat/waffleChat/urls.py
+++ 04/assignment/waffleChat/waffleChat/urls.py
@@ -13,7 +13,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('chat/', include("chat.urls")),
     path('__debug__/', include("debug_toolbar.urls")),

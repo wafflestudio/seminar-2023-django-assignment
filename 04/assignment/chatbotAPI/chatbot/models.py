fc8082214038604085cd85c070a053754fa6ed44
Yi Daeun <dawnayi2002@gmail.com>
2023-12-14 02:41:18 +0900

    create models

--- 04/assignment/chatbotAPI/chatbot/models.py
+++ 04/assignment/chatbotAPI/chatbot/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Character(models.Model):
+    image = models.ImageField(upload_to='images/')
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    first_message = models.CharField(max_length=200)
+
+    def get_first_message(self):
+        return self.first_message
+
+
+class Chat(models.Model):
+    ASSISTANT = 'A'
+    USER = 'U'
+
+    ROLE_CHOICES = [
+        (ASSISTANT, 'assistant'),
+        (USER, 'user')
+    ]
+    role = models.CharField(max_length = 10, choices=ROLE_CHOICES)
+    content = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content

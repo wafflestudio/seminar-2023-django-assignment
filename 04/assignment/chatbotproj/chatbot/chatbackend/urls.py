1fa2e68b3a933f72087d6c15e08311a18bf5e686
gs20118 <69315363+gs20118@users.noreply.github.com>
2023-12-13 22:06:36 +0900

    Delete 04/assignment/chatbotproj directory

--- 04/assignment/chatbotproj/chatbot/chatbackend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import characterView, chatView, chatDeleteView
-
-urlpatterns = [
-    path('character/', characterView.as_view()),
-    path('chats/', chatView.as_view()),
-    path('chats/delete-all/', chatDeleteView.as_view()),
-]

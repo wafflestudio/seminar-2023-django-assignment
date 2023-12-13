41858cd90a8f57d73d1dc9dcb25909031954caad
gs20118 <69315363+gs20118@users.noreply.github.com>
2023-12-13 21:10:55 +0900

    최유찬 과제 04 제출

---
+++ 04/assignment/chatbotproj/chatbot/chatbackend/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from .models import Character, Chat
+from .serializer import CharacterSerializer, ChatSerializer
+from rest_framework.response import Response
+from rest_framework.views    import APIView
+from rest_framework import status
+import openai
+import os
+
+openai.api_key = "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"
+
+class characterView(APIView):
+    def get(self, request):
+        object = Character.objects.get(pk=1)
+        serializer = CharacterSerializer(object)
+        return Response(serializer.data)
+
+class chatView(APIView):
+    def get(self, request):
+        object = Chat.objects.all()
+        serializer = ChatSerializer(object, many = True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ChatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(role = 'user')
+            
+            completion = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo",
+                messages = [
+                    {"role": "system", "content": "넌 쿠루미야! 대답은 무조건 감성적으로 해야 해"},
+                    {"role":"user", "content":request.data['content']}
+                ]
+            )
+            
+            content = completion.choices[0].message.content         
+            newchat = Chat.objects.create(role = 'assistant', content = content)
+            newchat.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class chatDeleteView(APIView):
+    def delete(self, request):
+        Chat.objects.all().delete()
+        content = '안녕 난 쿠루미야'         
+        newchat = Chat.objects.create(role = 'assistant', content = content)
+        newchat.save()
+        return Response(status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        Chat.objects.all().delete()
+        content = '안녕 난 쿠루미야'         
+        newchat = Chat.objects.create(role = 'assistant', content = content)
+        newchat.save()
+        return Response(status=status.HTTP_200_OK)
+
